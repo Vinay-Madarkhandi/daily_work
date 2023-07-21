3406a6f75e953b7edefcdb8ade3865a98873a2fb
Madarkhandi Vinay Shantappa <139522065+Vinay-Madarkhandi@users.noreply.github.com>
2023-07-21 11:42:21 +0530

    Add files via upload

---
+++ my daily work 3/index.js
@@ -0,0 +1,19 @@
+var count = 0 ;
+document.getElementById("incrase").onclick = function(){
+    count += 1 ;
+    document.getElementById("countNumber").innerHTML = count
+}
+document.getElementById("reset").onclick = function(){
+    count = 0 ;
+    document.getElementById("countNumber").innerHTML = count;
+}
+document.getElementById("decrease").onclick = function(){
+    count -= 1 ;
+    document.getElementById("countNumber").innerHTML = count;
+}
+var names ;
+document.getElementById("Submit").onclick = function (){
+    
+    names = document.getElementById("inputName").value ;
+    document.getElementById("msgPara").innerHTML = "Thank You " + names + " for Visiting Us!" ;
+}
